f89e66d84c6a3e038d37161de0f00030899bfd40
Ashis Sarthak Singh <Ashissarthak.singh@refinitiv.com>
2020-09-14 12:28:21 +0530

    Adding the build folder

---
+++ build/precache-manifest.505e9165497db40ad8455cbdb700ebf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96e1f157ba7ae8dbc45dbf61b9fc522",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95b094ca8a41f7183708",
+    "url": "/static/css/main.97b037da.chunk.css"
+  },
+  {
+    "revision": "bf170a5548f63c88f58f",
+    "url": "/static/js/2.8a8a405a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8a8a405a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95b094ca8a41f7183708",
+    "url": "/static/js/main.95b563b6.chunk.js"
+  },
+  {
+    "revision": "ef9482a33432ddc2aee3",
+    "url": "/static/js/runtime-main.85514b1d.js"
+  }
+]);
